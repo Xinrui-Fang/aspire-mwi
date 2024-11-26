745f0701f717b201f6c3618b44b3b9cd5b82ab09
Xinrui Fang <xinrui.fang@keio.jp>
2024-11-26 14:00:44 +0900

    update: pipleline

--- init.sh
+++ init.sh
@@ -3,6 +3,11 @@
 # Target Directory
 TARGET_DIRECTORY="./" # root 
 
+mkdir -p "./assets"
+
+curl -o "./assets/script.js" "https://raw.githubusercontent.com/Xinrui-Fang/aspire-mwi/main/assets/script.js"
+curl -o "./assets/style.css" "https://raw.githubusercontent.com/Xinrui-Fang/aspire-mwi/main/assets/style.css"
+
 # Replace .css file for index.html in root
 find "$TARGET_DIRECTORY" -type f -name "*.html" -exec sed -i '' -E 's|href="\./_static/[^"]*"|href="\./assets/style.css"|g' {} +
 # Replace .css file for index.html in route folders
