eabbcd07799508f8ff8ca53c4c4f95cf017864a5
Xinrui Fang <xinrui.fang@keio.jp>
2024-11-26 13:01:01 +0900

    debug

--- init.sh
+++ init.sh
@@ -4,11 +4,11 @@
 TARGET_DIRECTORY="./" # root 
 
 # Replace .css file for index.html in root
-find "$TARGET_DIRECTORY" -type f -name "*.html" -exec sed -i '' -E 's|href="\./_static/[^"]*"|href="\./_static/style.css"|g' {} +
+find "$TARGET_DIRECTORY" -type f -name "*.html" -exec sed -i '' -E 's|href="\./_static/[^"]*"|href="\./assets/style.css"|g' {} +
 # Replace .css file for index.html in route folders
-find "$TARGET_DIRECTORY" -type f -name "*.html" -exec sed -i '' -E 's|href="\./\.\./_static/[^"]*"|href="\./\.\./_static/style.css"|g' {} +
+find "$TARGET_DIRECTORY" -type f -name "*.html" -exec sed -i '' -E 's|href="\./\.\./_static/[^"]*"|href="\./\.\./assets/style.css"|g' {} +
 # Replace .js for index.html in root
-find "$TARGET_DIRECTORY" -type f -name "*.html" -exec sed -i '' -E 's|src="\./_static/[^"]*"|href="\./_static/script.js"|g' {} +
+find "$TARGET_DIRECTORY" -type f -name "*.html" -exec sed -i '' -E 's|src="\./_static/[^"]*"|src="\./assets/script.js"|g' {} +
 # Replace interactivity module path
 find "$TARGET_DIRECTORY" -type f -name "*.js" -exec sed -i '' -E 's|import \* as e from "@wordpress/interactivity"|import * as e from "./../../interactivity/index.min.js"|g' {} +
 
